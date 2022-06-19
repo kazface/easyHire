644075b2048573352109b21c6778eeb50ca9a79b
Batyr <batyr200288@gmail.com>
2022-06-19 12:07:34 +0600

    added isEployeer

--- app/src/main/java/com/example/fasthire/SignUpActivity.kt
+++ app/src/main/java/com/example/fasthire/SignUpActivity.kt
@@ -4,11 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ProgressBar
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.widget.doOnTextChanged
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
@@ -41,6 +39,7 @@ class SignUpActivity : AppCompatActivity() {
 
         var fullNameInput = findViewById<EditText>(R.id.fullNameInput)
         var phoneInput = findViewById<EditText>(R.id.phoneInput)
+        var isEmployerSwitch = findViewById<SwitchCompat>(R.id.isEmployerSwitch)
 
         var passwordInput = findViewById<EditText>(R.id.passwordInput)
         var passwordVerifyInput = findViewById<EditText>(R.id.passwordVerifyInput)
@@ -111,6 +110,7 @@ class SignUpActivity : AppCompatActivity() {
                             emailInput.text.toString(),
                             "+60 ${phoneInput.text.toString()}",
                             fullNameInput.text.toString(),
+                            isEmployerSwitch.isChecked
                         )
                             database
                                 .getReference("Users")
--- app/src/main/java/com/example/fasthire/User.kt
+++ app/src/main/java/com/example/fasthire/User.kt
@@ -6,14 +6,15 @@ class User() {
         this.email = email;
     }
 
-    constructor(email: String, phone: String, fullName: String) : this() {
+    constructor(email: String, phone: String, fullName: String, isEmployer: Boolean) : this() {
         this.email = email;
         this.phone = phone
         this.fullName = fullName
+        this.isEmployer = isEmployer
     }
 
 
-
+    final var isEmployer: Boolean = false;
     final var email: String = ""
     final var phone: String = ""
 
