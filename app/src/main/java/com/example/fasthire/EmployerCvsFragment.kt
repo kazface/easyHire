41f4912cc186cf67c08f196771578d92487e79b5
Batyr <batyr200288@gmail.com>
2022-07-02 16:38:45 +0600

    Employers

---
+++ app/src/main/java/com/example/fasthire/EmployerCvsFragment.kt
@@ -0,0 +1,123 @@
+package com.example.fasthire
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.facebook.shimmer.ShimmerFrameLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+
+
+class EmployerCvsFragment : Fragment() {
+    private lateinit var cvRecyclerView: RecyclerView;
+    private lateinit var cvList: ArrayList<Cv>
+    private lateinit var cvAdapter: CvAdapter
+    private lateinit var database: DatabaseReference
+    private lateinit var user: User
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            user = it.getSerializable("User") as User
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        cvRecyclerView = view.findViewById(R.id.cvRecyclerView)
+        var shimmer = view.findViewById<ShimmerFrameLayout>(R.id.cvShimmerLayout)
+        shimmer.startShimmerAnimation()
+        shimmer.visibility = View.VISIBLE
+        var fabAddCV = view.findViewById<FloatingActionButton>(R.id.fabAddCV)
+
+        cvRecyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL ,false)
+        cvRecyclerView.setHasFixedSize(true)
+        cvRecyclerView.visibility = View.INVISIBLE
+
+        cvList = arrayListOf();
+
+        cvAdapter = CvAdapter(view.context, cvList, false)
+
+        cvRecyclerView.adapter = cvAdapter
+
+        database = Firebase.database("https://fasthire-ae6c0-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Cvs")
+        var userCvsRef = FirebaseAuth.getInstance().uid?.let { database.child(it) }
+        userCvsRef!!.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    for(cvSnapshot in snapshot.children){
+                        var cv: Cv = cvSnapshot.getValue<Cv>()!!
+                        cvList.add(cv)
+                    }
+                    cvRecyclerView.visibility = View.VISIBLE
+                    shimmer.stopShimmerAnimation()
+                    shimmer.visibility = View.GONE
+                    cvAdapter.notifyDataSetChanged()
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+        cvAdapter.notifyDataSetChanged()
+
+
+        cvAdapter.onItemClick = { cv: Cv, bitmap: Bitmap ->
+            val cvDetailedFragment = CvDetailedFragment()
+            val transaction = fragmentManager?.beginTransaction()
+            val bundle = Bundle()
+            bundle.putParcelable("CV", cv)
+            bundle.putParcelable("CVPhoto", bitmap)
+            cvDetailedFragment.arguments = bundle
+            transaction?.replace(R.id.fragmentContainer, cvDetailedFragment)?.addToBackStack(null)
+            transaction?.commit()
+        }
+
+        fabAddCV.setOnClickListener{
+            val createApplicantCvPage = CreateApplicantCvPage()
+            val transaction = fragmentManager?.beginTransaction()
+            val bundle = Bundle()
+            bundle.putSerializable("User", user)
+            createApplicantCvPage.arguments = bundle
+            transaction?.setCustomAnimations(R.anim.slide_in_up, R.anim.slide_out_up);
+            transaction?.replace(R.id.fragmentContainer, createApplicantCvPage)?.addToBackStack(null)
+            transaction?.commit()
+        }
+
+
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_employer_jobs, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            EmployerCvsFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/fasthire/EmployerProfileFragment.kt
@@ -0,0 +1,192 @@
+package com.example.fasthire
+
+import android.app.Activity.RESULT_OK
+import android.app.ProgressDialog
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ApplicantProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class EmployerProfileFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private var user: User? = null
+    private val PICK_IMAGE_REQUEST = 71
+    lateinit var imageUri: Uri
+    lateinit var profilePhotoRef: StorageReference
+
+    private lateinit var userProfilePic: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            user = it.getSerializable("User") as User?
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var userFullNameText = view.findViewById<TextView>(R.id.userFullName)
+        var userEmail = view.findViewById<TextView>(R.id.userEmail)
+        var userPhone = view.findViewById<TextView>(R.id.userPhone)
+        var addImageButton = view.findViewById<Button>(R.id.addImageButton)
+        var progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
+        val storage = FirebaseStorage.getInstance()
+        var exitButton = view.findViewById<AppCompatButton>(R.id.exitButton)
+        var postedWorksButton = view.findViewById<AppCompatButton>(R.id.postedWorksButton)
+
+
+
+        profilePhotoRef = storage.getReferenceFromUrl("gs://fasthire-ae6c0.appspot.com/profilePhotos/${user!!.email}")
+        userProfilePic = view.findViewById<ImageView>(R.id.userProfilePic)
+        progressBar.visibility = View.VISIBLE
+        userProfilePic.visibility = View.INVISIBLE
+
+        GlideApp.with(requireView().context)
+            .load(profilePhotoRef)
+            .apply(RequestOptions())
+            .dontTransform()
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .placeholder(R.drawable.profile_pic_gray)
+            .listener(object : RequestListener<Drawable> {
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: com.bumptech.glide.request.target.Target<Drawable>?,
+                    dataSource: com.bumptech.glide.load.DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    progressBar.visibility = View.GONE
+                    userProfilePic.visibility = View.VISIBLE
+                    return false;
+                }
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    progressBar.visibility = View.GONE
+                    userProfilePic.visibility = View.VISIBLE
+                    return false;
+                }
+            })
+            .into(userProfilePic)
+
+
+        userFullNameText.text = user!!.fullName
+        userEmail.text = user!!.email
+        userPhone.text = user!!.phone
+
+        addImageButton.setOnClickListener{
+
+            launchGallery()
+        }
+
+        postedWorksButton.setOnClickListener{
+
+            val employerCvsFragment = EmployerCvsFragment()
+            val transaction = fragmentManager?.beginTransaction()
+            val bundle = Bundle()
+            bundle.putSerializable("User", user)
+            employerCvsFragment.arguments = bundle
+            transaction?.replace(R.id.fragmentContainer, employerCvsFragment)?.addToBackStack(null)
+            transaction?.commit()
+
+        }
+
+        exitButton.setOnClickListener{
+            val intent = Intent(activity, SignInActivity::class.java)
+            startActivity(intent)
+            FirebaseAuth.getInstance().signOut()
+        }
+    }
+
+    private fun launchGallery() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, 100)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 100 && resultCode ==  RESULT_OK){
+            imageUri = data?.data!!
+            uploadToFirebaseStorage()
+            userProfilePic.setImageURI(imageUri)
+        }
+
+
+    }
+
+
+
+    private fun uploadToFirebaseStorage(){
+        val progressBar = ProgressDialog(activity)
+        progressBar.setMessage("Uploading...")
+        progressBar.setCancelable(false)
+        progressBar.show()
+        profilePhotoRef.putFile(imageUri).addOnSuccessListener {
+            progressBar.hide()
+        }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_employer_profile, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment applicantProfileFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+            EmployerProfileFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
